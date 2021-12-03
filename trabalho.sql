53b61d6945bca169b4c4552e11a0bf76b234cc54
Bruno Jardim <61950932+StrillX@users.noreply.github.com>
2021-12-03 16:11:01 +0000

    final commit WICKED

--- trabalho.sql
+++ trabalho.sql
@@ -311,10 +311,11 @@ select * from venda;
 
 
 #TABELA DE COMPRAS DE UM CLIENTE, NESTE CASO O CLIENTE Nº 12
+create view comprasCliente_12 as
 select itens_de_venda.quantidade as Quantidade, medicamento.nome as Nome, medicamento.preco*quantidade as Preço,venda.data_venda as Data 
 	from medicamento,itens_de_venda,venda
     where medicamento.idMedicamento = itens_de_venda.Medicamento_idMedicamento and venda.Cliente_idCliente = 12 and  itens_de_venda.Venda_idVenda = venda.idVenda;
-    
+select * from comprasCliente_12;
 #TABELA DE VENDAS DE UM DETERMINADO FARMACEUTICO
 select itens_de_venda.quantidade as Quantidade, medicamento.nome as Nome,medicamento.preco*quantidade as Preço,venda.data_venda as Data
 	from medicamento,itens_de_venda,venda
@@ -325,17 +326,19 @@ select itens_de_venda.quantidade as Quantidade, medicamento.nome as Nome,medicam
 select * from medicamento;
 
 #TABELA DE VENDAS NUM DIA
+create view dia5_11_21 as
 select itens_de_venda.quantidade as Quantidade, medicamento.nome as Nome,medicamento.preco*quantidade as Preço,venda.data_venda as Data, cliente.nif as NIF
 	from medicamento,itens_de_venda,venda,cliente
     where medicamento.idMedicamento = itens_de_venda.Medicamento_idMedicamento and venda.data_venda = '2021-11-05' and  itens_de_venda.Venda_idVenda = venda.idVenda
     group by idVenda;
-
+select * from dia5_11_21;
 #TABELA DE CLIENTES DE UM DETERMINADA LOCALIDADE
 select cliente.nome AS Nome , cliente.nif as NIF
 	from cliente
     where cliente.localidade = 'Edral';
 
 #TABELA DE RANKING DE LUCRO DE VENDAS DOS FUNCIONARIOS
+create view ranking as
 select funcionario.nome as Nome, sum(medicamento.preco*itens_de_venda.quantidade)  as Total
 	from funcionario,itens_de_venda,medicamento,venda 
     where medicamento.idMedicamento = itens_de_venda.Medicamento_idMedicamento 
@@ -343,7 +346,7 @@ select funcionario.nome as Nome, sum(medicamento.preco*itens_de_venda.quantidade
     and funcionario.idFuncionario = venda.Funcionario_idFuncionario
     
     group by funcionario.nome;
-    
+select * from ranking;
 
     
     
